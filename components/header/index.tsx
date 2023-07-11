213712e3a306cae5d4bbe740b05e402bb32087f3
jane <jianqiang@gantsoftware.com>
2023-07-11 13:56:24 +0800

    新建 monorepo-demo

---
+++ components/header/index.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>1</div>
+}
+
+export default Header
---
+++ packages/demo/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@monorepo-demo/components"
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Yay! Welcome to umi!</h2>
+      <Header />
+      <p>
+        To get started, edit <code>pages/index.tsx</code> and save to reload.
+      </p>
+    </div>
+  )
+}
